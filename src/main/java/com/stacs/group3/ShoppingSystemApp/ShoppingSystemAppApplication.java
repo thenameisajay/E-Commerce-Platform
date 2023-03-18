7cb6f2cbc629a81c18013e9a575238d0e3a52efe
apm30 <m.ajaypradeep@icloud.com>
2023-03-18 17:12:17 +0000

    initial commit

---
+++ src/main/java/com/stacs/group3/ShoppingSystemApp/ShoppingSystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.stacs.group3.ShoppingSystemApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingSystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingSystemAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/stacs/group3/ShoppingSystemApp/view/CommandLine.java
@@ -0,0 +1,14 @@
+package com.stacs.group3.ShoppingSystemApp.view;
+
+public class CommandLine {
+
+    public static void main() {
+        run();
+    }
+
+    public static void run() {
+        System.out.println("Hello World from CommandLine");
+    }
+
+
+}
