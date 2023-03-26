c7055cc16524a58d9c9a1ebd8ce83d0074f77db2
apm30 <m.ajaypradeep@icloud.com>
2023-03-26 12:31:46 +0100

    uploading OrderService.java and OrderServiceTest.java.

--- src/test/java/com/stacs/group3/ShoppingSystemApp/serviceTests/OrderServiceTest.java
+++ src/test/java/com/stacs/group3/ShoppingSystemApp/serviceTests/OrderServiceTest.java
@@ -1,61 +1,62 @@
-//package com.stacs.group3.ShoppingSystemApp.serviceTests;
-//
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//
-//public class OrderServiceTest {
-//    OrderService orderService = new OrderService();
-//
-//    @BeforeEach
-//    public void setup() {
-//
-//        orderService.addOrder(1, "1", "Harry Potter: Philosopher's Stone",
-//                12.99f, 1, 12.99f, "mj456", "js123");
-//        orderService.addOrder(2, "3", "Educated",
-//                9.99f, 10, 99.9f, "jj789", "js123");
-//        orderService.addOrder(3, "2", "A Man Called Ove",
-//                6.99f, 100, 699f, "mj456", "hs123");
-//    }
-//
-//    @Test
-//    public void testAddOrder() {
-//        assertEquals(orderService.viewAllOrders(), 3);
-//
-//        orderService.addOrder(4, "2", "A Man Called Ove",
-//                6.99f, 2, 13.98f, "mj456", "js123");
-//
-//        assertEquals(orderService.viewAllOrders(), 4);
-//
-//        orderService.addOrder(5, "2", "A Man Called Ove",
-//                6.99f, 2, 13.98f, "mj456", "js123");
-//        orderService.addOrder(6, "2", "A Man Called Ove",
-//                6.99f, 2, 13.98f, "mj456", "js123");
-//
-//        assertEquals(orderService.viewAllOrders(), 6);
-//    }
-//
-//    @Test
-//    public void testGetOrderByCustomer() {
-//        assertEquals(orderService.getOrderByCustomer("js123").size(), 2);
-//        assertEquals(orderService.getOrderByCustomer("hs123").size(), 1);
-//    }
-//
-//    @Test
-//    public void testGetOrderBySeller() {
-//        assertEquals(orderService.getOrderBySeller("mj456").size(), 2);
-//        assertEquals(orderService.getOrderBySeller("jj789").size(), 1);
-//    }
-//
-//    @Test
-//    public void testViewAllOrders() {
-//        assertEquals(orderService.viewAllOrders(), 3);
-//        orderService.wipeAll();
-//        assertEquals(orderService.viewAllOrders(), 0);
-//
-//
-//    }
-//}
+package com.stacs.group3.ShoppingSystemApp.serviceTests;
+
+
+import com.stacs.group3.ShoppingSystemApp.service.OrderService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class OrderServiceTest {
+    OrderService orderService = new OrderService();
+
+    @BeforeEach
+    public void setup() {
+
+        orderService.addOrder(1, "1", "Harry Potter: Philosopher's Stone",
+                12.99f, 1, 12.99f, "mj456", "js123");
+        orderService.addOrder(2, "3", "Educated",
+                9.99f, 10, 99.9f, "jj789", "js123");
+        orderService.addOrder(3, "2", "A Man Called Ove",
+                6.99f, 100, 699f, "mj456", "hs123");
+    }
+
+    @Test
+    public void testAddOrder() {
+        assertEquals(orderService.viewAllOrders(), 3);
+
+        orderService.addOrder(4, "2", "A Man Called Ove",
+                6.99f, 2, 13.98f, "mj456", "js123");
+
+        assertEquals(orderService.viewAllOrders(), 4);
+
+        orderService.addOrder(5, "2", "A Man Called Ove",
+                6.99f, 2, 13.98f, "mj456", "js123");
+        orderService.addOrder(6, "2", "A Man Called Ove",
+                6.99f, 2, 13.98f, "mj456", "js123");
+
+        assertEquals(orderService.viewAllOrders(), 6);
+    }
+
+    @Test
+    public void testGetOrderByCustomer() {
+        assertEquals(orderService.getOrderByCustomer("js123").size(), 2);
+        assertEquals(orderService.getOrderByCustomer("hs123").size(), 1);
+    }
+
+    @Test
+    public void testGetOrderBySeller() {
+        assertEquals(orderService.getOrderBySeller("mj456").size(), 2);
+        assertEquals(orderService.getOrderBySeller("jj789").size(), 1);
+    }
+
+    @Test
+    public void testViewAllOrders() {
+        assertEquals(orderService.viewAllOrders(), 3);
+        orderService.wipeAll();
+        assertEquals(orderService.viewAllOrders(), 0);
+
+
+    }
+}
