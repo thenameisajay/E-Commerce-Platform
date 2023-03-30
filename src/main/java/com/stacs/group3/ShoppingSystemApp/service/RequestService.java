b4f5678f861a19d65b4efb9ca1ecf8fa39efbca1
apm30 <m.ajaypradeep@icloud.com>
2023-03-30 12:32:27 +0100

    Finishing JavaDoc for RequestService.java.

--- src/main/java/com/stacs/group3/ShoppingSystemApp/service/RequestService.java
+++ src/main/java/com/stacs/group3/ShoppingSystemApp/service/RequestService.java
@@ -6,9 +6,19 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class is used to create a request service.
+ * The service is for the request object.
+ */
 public class RequestService implements Serializable {
     private Map<String, Request> requests = new HashMap<>();
 
+    /**
+     * The method is used to store a request in the system.
+     *
+     * @param username    username of the user
+     * @param requestType type of request
+     */
     public void storeRequest(String username, String requestType) {
         if (requests.containsKey(username)) {
             throw new IllegalArgumentException("A Previous Request already exists and admin has not reviewed it yet.");
@@ -18,6 +28,11 @@ public class RequestService implements Serializable {
 
     }
 
+    /**
+     * This method is used to get all the requests for the admin to review.
+     *
+     * @return a map of all the requests
+     */
     public Map<String, Request> getRequests() {
         Map<String, Request> returnRequests = new HashMap<>();
         returnRequests.putAll(requests);
@@ -26,6 +41,11 @@ public class RequestService implements Serializable {
     }
 
 
+    /**
+     * This method is used to save the request data to a file.
+     *
+     * @throws IOException if the file is not found
+     */
     public void saveData() throws IOException {
         FileOutputStream f = new FileOutputStream("src/main/resources/data/requestData.ser");
         ObjectOutputStream o = new ObjectOutputStream(f);
@@ -34,6 +54,12 @@ public class RequestService implements Serializable {
         f.close();
     }
 
+    /**
+     * This method is used to load the request data from a file.
+     *
+     * @throws ClassNotFoundException if the class is not found
+     * @throws IOException            if the file is not found
+     */
     public void loadData() {
         try {
             FileInputStream fi = new FileInputStream("src/main/resources/data/requestData.ser");
@@ -47,6 +73,9 @@ public class RequestService implements Serializable {
         }
     }
 
+    /**
+     * This method is used to wipe all the request data.
+     */
     public void wipeAll() {
         requests.clear();
     }
