e45123742688ce65f125ed1cf898dbf64efb9225
Xunhao Jiang <xj20@st-andrews.ac.uk>
2023-03-30 18:23:07 +0100

    fixed the cross origin problem

--- src/main/java/com/stacs/group3/ShoppingSystemApp/ShoppingSystemAppApplication.java
+++ src/main/java/com/stacs/group3/ShoppingSystemApp/ShoppingSystemAppApplication.java
@@ -2,7 +2,9 @@ package com.stacs.group3.ShoppingSystemApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 public class ShoppingSystemAppApplication {
 
