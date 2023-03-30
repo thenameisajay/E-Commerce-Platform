0c5f487715c52db7e64cb3cf0b74e1eed8f3ccfd
apm30 <m.ajaypradeep@icloud.com>
2023-03-30 12:22:31 +0100

    finished JavaDoc for Model , finishing JavaDoc for CartService.java

--- src/main/java/com/stacs/group3/ShoppingSystemApp/service/CartService.java
+++ src/main/java/com/stacs/group3/ShoppingSystemApp/service/CartService.java
@@ -7,10 +7,20 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class is used to create a cart service.
+ * The Cart Service is for the cart object.
+ */
 public class CartService implements Serializable {
 
+    // Create a private Map of type <Integer, Cart> to store the cart objects
     private Map<Integer, Cart> cart = new HashMap<>();
 
+    /**
+     * This method is used to save the cart data to a file.
+     *
+     * @throws IOException if the file is not found
+     */
     public void saveData() throws IOException {
         FileOutputStream f = new FileOutputStream("src/main/resources/data/cartData.ser");
         ObjectOutputStream o = new ObjectOutputStream(f);
@@ -19,6 +29,12 @@ public class CartService implements Serializable {
         f.close();
     }
 
+    /**
+     * This method is used to load the cart data from a file.
+     *
+     * @throws ClassNotFoundException if the class is not found
+     * @throws IOException            if the file is not found
+     */
     public void loadData() {
         try {
             FileInputStream fi = new FileInputStream("src/main/resources/data/cartData.ser");
@@ -32,11 +48,18 @@ public class CartService implements Serializable {
         }
     }
 
+    /**
+     * This method is used to wipe all the cart data.
+     */
     public void wipeAll() {
         cart.clear();
     }
 
 
+    /**
+     * @param userName The username of the customer
+     * @return A map of the cart objects that belong to the customer
+     */
     public Map<Integer, Cart> viewCart(String userName) {
         Map<Integer, Cart> cartView = new HashMap<>();
         for (Map.Entry<Integer, Cart> entry : cart.entrySet()) {
@@ -49,6 +72,18 @@ public class CartService implements Serializable {
     }
 
 
+    /**
+     * THis method is used to add a product to the cart.
+     *
+     * @param cartID          The ID of the cart object
+     * @param productID       The ID of the product
+     * @param customerName    The userName of the customer
+     * @param productName     The name of the product
+     * @param productPrice    The price of the product
+     * @param productQuantity The quantity of the product
+     * @param productTotal    The total price of the product
+     * @param sellerName      The UserName of the seller
+     */
     public void addToCart(int cartID, String productID, String customerName, String productName, float productPrice, int productQuantity, float productTotal, String sellerName) {
         // If product ID already exists in cart, throw argument to delete the old one and add the new one
         for (Map.Entry<Integer, Cart> entry : cart.entrySet()) {
@@ -69,6 +104,11 @@ public class CartService implements Serializable {
         }
     }
 
+    /**
+     * This method is used to delete a product from the cart.
+     *
+     * @param cartID The ID of the cart object
+     */
     public void deleteItemFromCart(String cartID) {
         // First check if cartID is not empty
         if (cartID.isEmpty() | cartID == null) {
@@ -86,6 +126,11 @@ public class CartService implements Serializable {
         }
     }
 
+    /**
+     * This method is used to clear the cart.
+     *
+     * @param customerName The username of the customer
+     */
     public void clearCart(String customerName) {
         // First check if cartID is not empty
         if (customerName.isEmpty() | customerName == null) {
@@ -99,6 +144,11 @@ public class CartService implements Serializable {
         }
     }
 
+    /**
+     * This method is called to view all the objects in the cart.
+     *
+     * @return A collection of the cart objects
+     */
     public Collection<Cart> viewAllCarts() {
         if (cart.isEmpty()) {
             throw new IllegalArgumentException("No cart");
