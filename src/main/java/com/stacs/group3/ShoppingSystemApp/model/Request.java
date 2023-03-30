3b7ccbc72395551fbb357cd14a32e82b63bf7aed
apm30 <m.ajaypradeep@icloud.com>
2023-03-30 12:17:25 +0100

    reformat and javadoc for Request.java

--- src/main/java/com/stacs/group3/ShoppingSystemApp/model/Request.java
+++ src/main/java/com/stacs/group3/ShoppingSystemApp/model/Request.java
@@ -2,6 +2,13 @@ package com.stacs.group3.ShoppingSystemApp.model;
 
 import java.io.Serializable;
 
+/**
+ * This class is used to create a request object.
+ * It contains the following attributes:
+ * username, requestType
+ * It also contains the following methods:
+ * getUsername, setUsername, getRequestType, setRequestType
+ */
 public class Request implements Serializable {
     private String username;
     private String requestType;
